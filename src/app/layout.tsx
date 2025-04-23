bc270b7890de46d3257b80eb0487fe66811cfc0f
Nikola Tomovic <ntomovic92@gmail.com>
2025-04-23 15:28:34 +0200

    SEO optimisations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='sr'>
       <head>
+        <link rel='canonical' href='https://www.ugradnjaeurokuka.com' />
         <link rel='icon' href='/favicon.ico' sizes='any' />
         <link rel='icon' href='/favicon-16x16.png' type='image/png' sizes='16x16' />
         <link rel='icon' href='/favicon-32x32.png' type='image/png' sizes='32x32' />
