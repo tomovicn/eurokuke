89af1985d0b6b66abc201e63402ed79eb204c81f
Nikola Tomovic <ntomovic92@gmail.com>
2026-09-04 21:05:07 +0200

    Update i18n.ts

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -15,7 +15,6 @@ export function useTranslation() {
       }
     }
 
-
     if (typeof value === 'string' && params) {
       return value.replace(/\{(\w+)\}/g, (_, param) => params[param] || '');
     }
